f47d18e7c932543540afd868c9cc079e14d5c149
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-05-23 15:34:02 +0300

    init: start project

---
+++ src/pages/api/hello.ts
@@ -0,0 +1,5 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function hello(req: NextApiRequest, res: NextApiResponse) {
+  return res.status(200).json({ name: "John Doe" });
+}
