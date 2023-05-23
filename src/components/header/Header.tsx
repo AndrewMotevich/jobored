702ec8f8b202c8de6adf21c3c588b8932ae78259
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-05-23 22:46:34 +0300

    feat: style header components

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,38 @@
 "use client";
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import icon from "../../../public/icons/logo.svg";
 
 const Header = () => {
+  const pathname = usePathname();
   return (
     <header>
-      <h1>
-        <Link href="/">Jobored</Link>
-      </h1>
-      <Link href="/">Поиск вакансий</Link>
-      <Link href="/favorites">Избранное</Link>
+      <div className="header-container">
+        <nav>
+          <h1>
+            <Image src={icon} width={30} height={30} alt="Logo" />
+            <Link href="/">Jobored</Link>
+          </h1>
+          <ul>
+            <li>
+              <Link href="/" className={pathname === "/" ? "active" : ""}>
+                <h3 className={pathname === "/" ? "active" : ""}>
+                  Поиск вакансий
+                </h3>
+              </Link>
+            </li>
+            <li>
+              <Link href="/favorites">
+                <h3 className={pathname === "/favorites" ? "active" : ""}>
+                  Избранное
+                </h3>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
