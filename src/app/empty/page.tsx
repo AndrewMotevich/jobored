96d520b1e84b925dafec518178d4185624de6f91
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-05-25 03:35:35 +0300

    feat: add empty page

--- src/app/empty/page.tsx
+++ src/app/empty/page.tsx
@@ -1,7 +1,22 @@
+"use client";
 import React from "react";
+import Image from "next/image";
+import seeker from "../../../public/images/seeker.png";
+import { Button } from "@mantine/core";
+import Link from "next/link";
 
 const EmptyPage = () => {
-  return <div>EmptyPage</div>;
+  return (
+    <div className="empty-wrapper">
+      <Image src={seeker} width={240} height={230} alt="Seeker" />
+      <p>Упс, здесь ничего нет!</p>
+      <Link href="/">
+        <Button size="xl" variant="light">
+          Поиск вакансий
+        </Button>
+      </Link>
+    </div>
+  );
 };
 
 export default EmptyPage;
