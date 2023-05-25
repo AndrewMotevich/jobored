48c3528b406d8b7408efdc9c314e90f4db745fed
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-05-25 04:56:17 +0300

    fix: add env variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-// import Header from "@/components/Header/Header";
+import Header from "@/components/header/Header";
 import "../sass/global.scss";
 import { Inter } from "next/font/google";
 import { Metadata } from "next";
